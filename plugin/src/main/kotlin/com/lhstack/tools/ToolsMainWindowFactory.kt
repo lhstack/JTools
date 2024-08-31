4f32aa0ed5a4dd9f2fcdcbcdc69ee6b9a61b3aa6
lhstack <lhstack@foxmail.com>
2024-08-31 23:31:03 +0800

    Tools插件卸载重新安装未触发openProject逻辑

--- plugin/src/main/kotlin/com/lhstack/tools/ToolsMainWindowFactory.kt
+++ plugin/src/main/kotlin/com/lhstack/tools/ToolsMainWindowFactory.kt
@@ -1,14 +1,31 @@
 package com.lhstack.tools
 
+import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import com.lhstack.tools.ext.notify
+import com.lhstack.tools.listener.ProjectPluginListener
 import com.lhstack.tools.plugins.pluginManager
 
 class ToolsMainWindowFactory : ToolWindowFactory {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        this.pluginManager().installs { _, _, _, _ ->
-            //处理插件卸载重新安装的逻辑,不会走打开app和打开项目的回调
+        this.pluginManager().installs { pluginInfo, plugin, _, _ ->
+            ProjectManager.getInstance().openProjects.forEach { openProject ->
+                plugin.openProject(openProject) {
+                    if (plugin.isUIPlugin) {
+                        openProject.messageBus.syncPublisher(ProjectPluginListener.TOPIC)
+                            .openPanel(pluginInfo, plugin)
+                    } else {
+                        openProject.notify(
+                            "插件点击通知",
+                            "此插件不是UI插件,不存在面板",
+                            NotificationType.WARNING
+                        )
+                    }
+                }
+            }
         }
         //插件重新安装处理
         this.pluginManager().add(project.locationHash)
