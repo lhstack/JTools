e51b48454f8fa0056d351261a1866e79722cd18c
lhstack <lhstack@foxmail.com>
2024-12-20 22:37:40 +0800

    v1.0.4

--- plugin/src/main/kotlin/com/lhstack/tools/JLoggerWindowFactory.kt
+++ plugin/src/main/kotlin/com/lhstack/tools/JLoggerWindowFactory.kt
@@ -1,7 +1,9 @@
 package com.lhstack.tools;
 
 import com.intellij.build.BuildTextConsoleView
+import com.intellij.execution.impl.ConsoleViewUtil
 import com.intellij.icons.AllIcons
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.editor.ex.EditorEx
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
@@ -9,21 +11,19 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.lhstack.tools.const.Const
 import com.lhstack.tools.const.Icons
 import com.lhstack.tools.ext.getConsoleLog
-import javax.swing.JScrollPane
 
-class JLoggerWindowFactory:ToolWindowFactory {
+class JLoggerWindowFactory : ToolWindowFactory {
 
     override fun init(toolWindow: ToolWindow) {
         val project = toolWindow.project
         val consoleView = BuildTextConsoleView(project, true, listOf())
+        //初始化
+        consoleView.component
         val editor = consoleView.editor
         editor?.let {
             val editorEx = it as EditorEx
             editorEx.settings.isUseSoftWraps = true
-            editorEx.scrollPane.horizontalScrollBarPolicy = JScrollPane.HORIZONTAL_SCROLLBAR_NEVER
         }
-        //初始化
-        consoleView.component
         project.putUserData(Const.LOG_CONSOLE_KEY,consoleView)
         toolWindow.setIcon(AllIcons.Toolwindows.ToolWindowRun)
     }
