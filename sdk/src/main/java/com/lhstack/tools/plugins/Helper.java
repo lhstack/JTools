6dccaceb604a00c94fa59160984f34cf7bc4b43a
lhstack <lhstack@foxmail.com>
2026-07-23 22:43:46 +0800

    chore(v1.1.0.5): 插件版本与 Helper 元数据回退为 1.1.0.5/1105

--- sdk/src/main/java/com/lhstack/tools/plugins/Helper.java
+++ sdk/src/main/java/com/lhstack/tools/plugins/Helper.java
@@ -47,7 +47,7 @@ public class Helper {
 
     public static Key<Logger> JTOOLS_SYS_LOGGER = Key.create("JTOOLS_SYS_LOGGER");
 
-    public static Integer JTOOLS_VERSION = 1106;
+    public static Integer JTOOLS_VERSION = 1105;
 
     private static final IdeInfo IDE_INFO;
 
